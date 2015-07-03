ca760f17ece9ab6816733f42165a15d8f783984d
unknown <ilganeli@gmail.com>
2015-07-03 00:44:11 -0700

    Things are updated.

--- pages/upcomingevents.php
+++ pages/upcomingevents.php
@@ -4,12 +4,13 @@
 		</div>
 	</div>
 <div class="grid_10 prefix_1" >
-	<span style="color:#3abdfe; font-size:16px;">New Record Label</span><br />
-	<p>The Greek Chamber Music Project is pleased to announce the launch of our very own record label. The goal is to spread the music of Greek composers around the world and to support the young artists who make this wonderful music. Our debut CD, titled <strong>Hellenic Song: A Musical Migration</strong>, is a collection of sonatas, suites, and popular songs by contemporary Greek composers featuring Mary-Victoria Voutsas (piano), Anna Matijasic Hennessy (violin), and Ellie Falaris Ganelin (flute). Join us in making the production of this CD and the new label a reality by <a href="https://www.indiegogo.com/projects/greek-chamber-music-project-label-cd-launch/x/10538062" class="link" target="_blank">pre-ordering our debut CD</a>!</p>
-  <p><iframe width="560" height="315" src="https://www.youtube.com/embed/-XRSAuE1YIg" frameborder="0" allowfullscreen></iframe></p>
 
-  <p>&nbsp;</p>
-  <p><span style="color:#3abdfe; font-size:16px;">CD Release &amp; European Month of Culture</span><br />
+	<p><a href="http://kennedy-center.org/explorer/videos/?id=M6323" class="link" target="_blank" >Watch the video of GCMP's CD release concert at the Kennedy Center.</a>
+	<p><a href="http://kennedy-center.org/explorer/videos/?id=M6323" target="_blank" ><img src="kc-screenshot.jpg" /></a></p>
+	
+	<hr width="100%" align="left" />
+	
+	<p><span style="color:#3abdfe; font-size:16px;">CD Release &amp; European Month of Culture</span><br />
 	<span style="font-size:12px;">
 	<strong>Monday, May 25 at 6 pm<br />
 	<a href="http://www.kennedy-center.org/explorer/artists/?entity_id=221268&source_type=B" class="link" target="_blank" >The Kennedy Center's Millennium Stage</a></strong><br />
@@ -22,8 +23,17 @@
       and cherished folk songs that highlight Greece’s rich musical repertoire.
   Compositions by Hadjidakis, Dragatakis, Benakis, Makris, and more.</p>
 		
-  <p><em>Presented in cooperation with the Embassy of Greece as part of the European Month of Culture.</em>
+  <p><em>Presented in cooperation with the Embassy of Greece as part of the European Month of Culture.</em></p>
+  <p><strong>Press coverage:</strong><a href="http://www.greeknewsonline.com/greek-chamber-music-project-launches-record-label-celebrating-chamber-music-from-greece/" class="link" >GreekNews, May 17, 2015</a> | <a href="https://hellenicnews.com/join-us-as-we-celebrate-european-month-of-culture-with-greek-chamber-music-project-at-the-john-f-kennedy/" class="link" >Hellenic News of America, May 19, 2015</a> | <a href="http://hellenicjournal.org/hj/publications/0515/index.html#17" class="link" >Hellenic Journal, May 2015</a>
   <p> &nbsp;</p> 
+	<hr width="100%" align="left" />
+	
+	<span style="color:#3abdfe; font-size:16px;">New Record Label</span><br />
+	<p>The Greek Chamber Music Project is pleased to announce the launch of our very own record label. The goal is to spread the music of Greek composers around the world and to support the young artists who make this wonderful music. Our debut CD, titled <strong>Hellenic Song: A Musical Migration</strong>, is a collection of sonatas, suites, and popular songs by contemporary Greek composers featuring Mary-Victoria Voutsas (piano), Anna Matijasic Hennessy (violin), and Ellie Falaris Ganelin (flute). Join us in making the production of this CD and the new label a reality by <a href="https://www.indiegogo.com/projects/greek-chamber-music-project-label-cd-launch/x/10538062" class="link" target="_blank">pre-ordering our debut CD</a>!</p>
+  <p><iframe width="560" height="315" src="https://www.youtube.com/embed/-XRSAuE1YIg" frameborder="0" allowfullscreen></iframe></p>
+
+  <p>&nbsp;</p>
+
 </div>
         <!--
         <div id="openingtext">
